12440c10c0a4c283c897c90de58767932612c86d
Eslam Mostafa <cseslam@gmail.com>
2017-06-28 05:38:07 +0200

    clean code

--- src/main.js
+++ src/main.js
@@ -8,16 +8,8 @@ require('bootstrap/dist/css/bootstrap.css')
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
 
-import VueSocketio from 'vue-socket.io'
-Vue.use(VueSocketio, 'http://localhost.com:3000')
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  sockets: {
-    connect () {
-      console.log('df')
-    }
-  },
   render: h => h(App)
 })
